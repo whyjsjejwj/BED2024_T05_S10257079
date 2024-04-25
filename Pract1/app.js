229123695c6956657af9e69456db12345bfeea29
whyjsjejwj <s10257079@connect.np.edu.sg>
2024-04-25 18:09:37 +0800

    commit

---
+++ Pract1/app.js
@@ -0,0 +1,7 @@
+const path = require('node:path');
+
+const notes = '/Users/65886/OneDrive/Pictures/Documents/Homework/BED/Pract1/data.txt'
+
+console.log(path.dirname(notes));
+console.log(path.basename(notes));
+console.log(path.extname(notes));
