be837d111c493c1763268fcd01c4af2347a4f1fd
whyjsjejwj <s10257079@connect.np.edu.sg>
2024-05-02 20:19:24 +0800

    commit

---
+++ Homework/BED/Week03/books-api-mvc/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const booksController = require("./controllers/booksController"); // Import controller
+const app = express();
+
+app.use(bodyParser.json()); // Parse incoming JSON data in request body
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+// Define individual routes for each controller function
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", booksController.createBook);
+app.put("/books/:id", booksController.updateBook);
+app.delete("/books/:id", booksController.deleteBook);
+
+
+const validateBook = require("./middlewares/validateBook");
+
+app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
+
+const port = process.env.PORT || port;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
